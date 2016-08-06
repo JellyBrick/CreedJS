2fc1898722c0d67b8dff92ebd06f0a65aa36bff7
물외한인 <dreamaker7770@gmail.com>
2016-08-07 04:48:45 +0900

    Delete models.js

--- src/models.js
+++
@@ -1,7 +0,0 @@
-var schema = require('./schema');
-var mongoose = require('mongoose');
-
-module.experts = {
-	User: mongoose.model('User', schema.user);
-	Server: mongoose.model('Server', schema.server);
-}
