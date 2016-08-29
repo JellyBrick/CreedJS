4f3322d3f5ec8113ae77a454c00f2e02b053a366
110EIm <dreamaker7770@gmail.com>
2016-08-29 23:53:43 +0900

    Implementing Server

--- src/Server.js
+++ src/Server.js
@@ -16,13 +16,21 @@ moudle.exports = class Server {
         this.changedUsers = {}
     }
 
+    get servers() {
+        return this.server;
+    }
+
+    get changedUsers() {
+        return this.changedUsers;
+    }
+
     /**
 	 * @description 플레이어가 서버에 접속할 때 호출됩니다.
      * @param {Server} server
      * @param {Player} player
      */
     onPlayerJoin(server, player) {
-
+        server.players.push(player);
     }
 
     /**
@@ -31,6 +39,22 @@ moudle.exports = class Server {
      * @param {Player} player
      */
     onPlayerQuit(server, player) {
+        server.players.splice(server.players.indexOf(player), 1);
+    }
 
+    /**
+	 * @description 서버가 켜질 때 호출됩니다.
+     * @param {Server} server
+     */
+    onServerOpen(server) {
+        this.servers.push(server);
+    }
+
+    /**
+     * @description 서버가 꺼질 때 호출됩니다.
+     * @param {Server} server
+     */
+    onServerClose(server) {
+        this.server.splice(this.server.indexOf(server), 1);
     }
 };
