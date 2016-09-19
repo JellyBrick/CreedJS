0258052ab9602ffb668b6389dabe45a2f5aa8d61
110EIm <dreamaker7770@gmail.com>
2016-09-20 04:07:29 +0900

    Remove outdated code.

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,6 @@ var router = express.Router();
 var Account = require('../src/Player');
 var Server = require('../src/Server');
 var limiter = require('../src/api/limiter');
-var Query = require('../src/Query');
 
 var User = require('../src/models/Account');
 
@@ -26,9 +25,7 @@ router.get('/auth/:nickname:password', (res, req) => {
  * @description  When player joins the server, you should use this api
  */
 router.post('/join', limiter.join, (req, res) => {
-    Query.findAccountByNickNameCached(req.body.nickname, reply => {
-        res.json(reply);
-    });
+
 });
 
 module.exports = router;
